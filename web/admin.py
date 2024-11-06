b49e92f5d14514ec94db7406bbdab80bf4e4e3e2
Luis Pena Herrera <raptor_luis@hotmail.com>
2024-11-06 15:06:31 -0300

    add admin site

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Flan
 
-admin.site.register(Flan)
+
+#admin.site.register(Flan)
 # Register your models here.
+@admin.register(Flan)
+class FlanAdmin(admin.ModelAdmin):
+    list_display = ('name','description','is_private')
+    list_display_links = ('name',)
+    search_fields = ('name','description')
+    list_per_page = 2
