5c0e390a3a8e335b7fca42ea64f470b399df9950
Luis Pena Herrera <raptor_luis@hotmail.com>
2024-11-08 14:36:12 -0300

    add banner

--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from django.contrib.auth import login,logout, authenticate
 # Create your tests here.
+import django
+django.settings.configure()
+django.setup()
+from django.contrib.auth.models import User
+user = authenticate( username='admin', password='123')
+print(user)
